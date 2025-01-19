1f06a4ac40a55ba6a1b5ccbf51d42a451ec0ee19
Mohit Kambli <mohitkambli8@gmail.com>
2025-01-19 03:36:26 -0500

    Updated Allowed REST methods

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config')  # Load app configuration
     CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "https://cover-craft-frontend.vercel.app/", "https://cover-craft-backend.vercel.app/generate-cover-letter"], "methods": ["GET", "POST", "PUT", "DELETE"]}})
-
     # Import and register blueprints (routes)
     from app.routes import api
     app.register_blueprint(api)
