b80f6b9dcde3a1042d184db49d0e51917bb41494
Mohit Kambli <mohitkambli8@gmail.com>
2025-01-18 20:09:03 -0500

    Updated HTTP methods for Backend

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config')  # Load app configuration
-    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "cover-craft-frontend.vercel.app"], "methods": ["GET", "POST", "PUT", "DELETE"]}})
+    CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "cover-craft-frontend.vercel.app"], "methods": ["POST"]}})
 
     # Import and register blueprints (routes)
     from app.routes import api
